680f8e83ab197054ad58a31a9f76be7071d37d1f
Rachel Wong <rwrachelwong@gmail.com>
2018-12-05 05:53:06 +0000

    Added comments for myself. And some prettying up in my code

--- game.rb
+++ game.rb
@@ -14,7 +14,7 @@ class Game
     while @players[0].lives > 0 && @players[1].lives > 0
       player = @current_player
       q1 = Question.new
-      puts "#{@current_player.name}: #{q1.ask_q}" 
+      puts "#{player.name}: #{q1.ask_q}" 
       playeranswer = $stdin.gets.chomp.to_i
       check_answer(playeranswer, q1)
     end
@@ -30,29 +30,35 @@ class Game
 
   def check_answer(playeranswer, q1)
     if q1.answer == playeranswer
-      puts "YES! That is correct"
+      puts "     YES! That is correct      "
       change_players
+      puts "\n----------- NEW TURN ----------"
       start_game
     else
-      puts "NO! That is incorrect"
+      puts "     NO! That is incorrect     "
       @current_player.lose_a_life
       puts "--------- SCORE COUNT ---------"
-      puts "Player 1: #{@players[0].lives}/3 vs. Player 2: #{@players[1].lives}/3"
+      puts "\n Player 1:#{@players[0].lives}/3 vs. Player 2:#{@players[1].lives}/3"
       if @current_player.alive?
         change_players
-        puts "----------- NEW TURN ----------"
+        puts "\n----------- NEW TURN ----------"
         start_game
       else
-        puts "----!!!!NO MORE LIVES!!!!----"
+        puts "-----!!!!NO MORE LIVES!!!!-----"
         game_over
       end
     end
   end
 
   def game_over
-    puts "--------- GAME OVER ---------"
+    puts "---------- GAME OVER ----------"
     exit(0)
   end 
 
 end
 
+
+# Most of the code will be here as the game is responsible for running...as a game. 
+# the players are initialised here so players are created when a new game starts. 
+# Not initialised in main.rb or players.rb, as we would need to pass the players into a new game instance.
+# Can be problematic if it is passed in more than 2 players. 
